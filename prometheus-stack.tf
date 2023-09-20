7b62239a387ef2761570a0c800856345d3593790
Oussama Ait Khouya <zakaria.ait.yakoub@gmail.com>
2023-09-20 11:27:31 +0100

    fix issue added provider

--- prometheus-stack.tf
+++ prometheus-stack.tf
@@ -1,7 +1,9 @@
+provider "helm" {}
+
 resource "helm_release" "prometheus-stack" {
   name       = "prometheus-stack"
 
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus-monitoring"
-  
+
 }
